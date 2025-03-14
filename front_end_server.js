110d04348f0b0778ddd3b2e5ae25467916881abd
jacoFixSmile <daanfostier@gmail.com>
2025-03-14 23:55:46 +0100

    lobby and starting game

--- front_end_server.js
+++ front_end_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const { Server } = require('socket.io');
-const  { Player, Game,Set } = require('./game'); 
+const { Player, Game, Set } = require('./game');
 const { createServer } = require('node:http');
 const { DatabaseSync } = require('node:sqlite');
 const app = express();
@@ -11,19 +11,27 @@ const csvFilePath = path.join(__dirname, 'public', 'users.csv');
 //socket
 const server = createServer(app);
 const io = new Server(server);
-
-// settings 
-var game;
+// database 
 const database = new DatabaseSync('public/poker.db');
+
+// settings game 
+var game;
 
 app.use(express.static('public'));
 app.use(express.json());
 
 // Route for the homepage
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    console.log("getting base URL")
+    if (game == null) {
+        res.sendFile(__dirname + '/Public/index2.html');
+    } else {
+        res.sendFile(__dirname + '/Public/lobby.html');
+
+    }
 });
 app.get('/admin', (req, res) => {
+    console.log("admin")
     res.sendFile(__dirname + '/Public/admin.html');
 });
 app.get('/lobby', (req, res) => {
@@ -45,10 +53,12 @@ function parseCSV(csvText) {
 // API route to get all players
 app.get('/players', (req, res) => {
     const query = database.prepare('SELECT * FROM users');
-     res.json(query.all())
+    res.json(query.all())
 
 });
 app.get('/start_game', (req, res) => {
+    console.log("trying to contact lobby")
+    io.emit('lobby', 'trying to contact lobby');
     demo_game = new Game('demo_game')
 
 });
@@ -57,45 +67,39 @@ app.post('/players', async (req, res) => {
     const { name } = req.body;
     if (!name) return res.status(400).json({ error: 'Name is required' });
     // protection against sql insert needed
-    const query = database.prepare(`SELECT * FROM users where name like '${name}'`); 
-    if(query.all().length==0) {
+    const query = database.prepare(`SELECT * FROM users where name like '${name}'`);
+    if (query.all().length == 0) {
         const insert = database.prepare('INSERT INTO users (name) VALUES (?)');
         insert.run(name);
-        const result = database.prepare(`SELECT * FROM users where name like '${name}'`); 
+        const result = database.prepare(`SELECT * FROM users where name like '${name}'`);
         res.status(201).json(result.all());
-    }else{
+    } else {
 
         return res.status(500).json({ error: 'Name is already in use' })
     }
-    
+
 });
 
 function ensureCSVExists() {
-  if (!fs.existsSync(csvFilePath)) {
-      console.log('CSV file not found. Creating a new one...');
-      const headers = 'id,name,chips';
-      fs.writeFileSync(csvFilePath, headers, 'utf8');
-  } else {
-  }
+    if (!fs.existsSync(csvFilePath)) {
+        console.log('CSV file not found. Creating a new one...');
+        const headers = 'id,name,chips';
+        fs.writeFileSync(csvFilePath, headers, 'utf8');
+    } else {
+    }
 }
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+        console.log('user disconnected');
     });
-  });
-// game logic with socket
+});
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-  });
 
 server.listen(3000, () => {
     ensureCSVExists()
 
-  console.log('Server running on http://localhost:3000');
+    console.log('Server running on http://localhost:3000');
 });
 
 
